9e69f4f04ea6e4195f7884237954b6a1a9e5b5a2
MANOJ <46713945+manojjayakar@users.noreply.github.com>
2024-06-08 19:11:44 +0530

    Create finonacci recursion.py

---
+++ finonacci recursion.py
@@ -0,0 +1,31 @@
+Practice Problem for Recursion:
+
+Problem Statement:
+
+The Fibonacci numbers, commonly denoted F(n) form
+a sequence, called the Fibonacci sequence, such that
+each number is the sum of the two preceding ones,
+starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2),for n > 1
+Given n, calculate F(n)
+
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+n= int(input())
+def f(n):
+  if n==1:
+    return 1
+  if n==0:
+    return 0
+
+  return f(n-1)+ f(n-2)
+print(f(n))
